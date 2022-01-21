e1da79aab770f834428c11010a0c8cc75bfa5628
ALFOLIcd <o.alff@mailbox.org>
2022-01-21 19:37:23 +0100

    Updated admin/words.js

--- admin/words.js
+++ admin/words.js
@@ -3,43 +3,123 @@
 
 systemDictionary = {
 	"signal-cmb adapter settings": {
-		"en": "Adapter settings for signal-cmb",
-		"de": "Adaptereinstellungen für signal-cmb",
+		"en": "signal-cmb adapter settings",
+		"de": "Signal-cmb-Adaptereinstellungen",
+		"ru": "настройки адаптера signal-cmb",
+		"pt": "configurações do adaptador signal-cmb",
+		"nl": "signaal-cmb adapter instellingen",
+		"fr": "paramètres de l'adaptateur signal-cmb",
+		"it": "impostazioni dell'adattatore signal-cmb",
+		"es": "configuración del adaptador de señal-cmb",
+		"pl": "ustawienia adaptera signal-cmb",
+		"zh-cn": "信号 cmb 适配器设置"
 	},
 	'API key': {
-        'en': 'API key',
-        'de': 'API-Schlüssel'
+        "en": "API key",
+		"de": "API-Schlüssel",
+		"ru": "ключ API",
+		"pt": "Chave API",
+		"nl": "API sleutel",
+		"fr": "Clé API",
+		"it": "Chiave API",
+		"es": "Clave API",
+		"pl": "Klucz API",
+		"zh-cn": "API 密钥"
     },
     'Default phone number': {
-        'en': 'Default phone number',
-        'de': 'Standardtelefonnummer'
+        "en": "Default phone number",
+		"de": "Standardtelefonnummer",
+		"ru": "Номер телефона по умолчанию",
+		"pt": "Número de telefone padrão",
+		"nl": "Standaard telefoonnummer",
+		"fr": "Numéro de téléphone par défaut",
+		"it": "Numero di telefono predefinito",
+		"es": "Número de teléfono predeterminado",
+		"pl": "Domyślny numer telefonu",
+		"zh-cn": "默认电话号码"
     },
     'You need to get the API key form the bot before using the API:': {
-        'en': 'You need to get the API key form the bot before using the API:',
-        'de': 'Sie müssen den API-Schlüssel vom Bot erhalten, bevor Sie die API verwenden können:'
+        "en": "You need to get the API key form the bot before using the API:",
+		"de": "Sie müssen den API-Schlüssel vom Bot abrufen, bevor Sie die API verwenden:",
+		"ru": "Вам необходимо получить ключ API от бота перед использованием API:",
+		"pt": "Você precisa obter a chave de API do bot antes de usar a API:",
+		"nl": "U moet de API-sleutel van de bot krijgen voordat u de API gebruikt:",
+		"fr": "Vous devez obtenir la clé API du bot avant d'utiliser l'API :",
+		"it": "È necessario ottenere la chiave API dal bot prima di utilizzare l'API:",
+		"es": "Debe obtener la clave API del bot antes de usar la API:",
+		"pl": "Musisz uzyskać klucz API z bota przed użyciem API:",
+		"zh-cn": "在使用 API 之前，您需要从机器人获取 API 密钥："
     },
-    '1. Add the phone number +34 698 28 89 73 into your Phone Contacts. (Name it it as you wish)': {
-        'en': '1. Add the phone number +34 698 28 89 73 into your Phone Contacts. (Name it it as you wish)',
-        'de': '1. Fügen Sie die Telefonnummer +34 698 28 89 73 zu Ihren Telefonkontakten hinzu. (Nennen Sie es wie Sie möchten)'
+    '1. Add the phone number +34 603 21 25 97 into your Phone Contacts. (Name it it as you wish)': {
+        "en": "1. Add the phone number +34 603 21 25 97 into your Phone Contacts. (Name it it as you wish)",
+		"de": "1. Fügen Sie die Telefonnummer +34 603 21 25 97 zu Ihren Telefonkontakten hinzu. (Nennen Sie es, wie Sie möchten)",
+		"ru": "1. Добавьте номер телефона +34 603 21 25 97 в свои телефонные контакты. (назовите его как хотите)",
+		"pt": "1. Adicione o número de telefone +34 603 21 25 97 em seus contatos do telefone. (nomeie como quiser)",
+		"nl": "1. Voeg het telefoonnummer +34 603 21 25 97 toe aan uw telefooncontacten. (noem het zoals je wilt)",
+		"fr": "1. Ajoutez le numéro de téléphone +34 603 21 25 97 dans vos contacts téléphoniques. (Nommez-le comme vous le souhaitez)",
+		"it": "1. Aggiungi il numero di telefono +34 603 21 25 97 nei tuoi Contatti del telefono. (chiamalo come desideri)",
+		"es": "1. Añade el número de teléfono +34 603 21 25 97 a tus Contactos telefónicos. (Nombralo como quieras)",
+		"pl": "1. Dodaj numer telefonu +34 603 21 25 97 do Kontaktów w telefonie. (Nazwij to, jak chcesz)",
+		"zh-cn": "1. 将电话号码 +34 603 21 25 97 添加到您的电话联系人中。 （随意命名）"
     },
     '2. Send this message `I allow callmebot to send me messages` (in english) to the new Contact created (using Signal of course)': {
-        'en': '2. Send this message <i>`I allow callmebot to send me messages`</i> (in english) to the new Contact created (using Signal of course)',
-        'de': '2. Senden Sie diese Nachricht <i>\'I allow callmebot to send me messages\'</i> (auf Englisch) an den neu erstellten Kontakt (natürlich mit Signal).'
+        "en": "2. Send this message `I allow callmebot to send me messages` (in english) to the new Contact created (using Signal of course)",
+		"de": "2. Senden Sie diese Nachricht „Ich erlaube Callmebot, mir Nachrichten zu senden“ (auf Englisch) an den neu erstellten Kontakt (natürlich mit Signal)",
+		"ru": "2. Отправьте это сообщение «Я разрешаю callmebot отправлять мне сообщения» (на английском языке) новому созданному контакту (конечно, используя Signal)",
+		"pt": "2. Envie esta mensagem `I allow callmebot to send me messages` (em inglês) para o novo contato criado (usando o Signal, é claro)",
+		"nl": "2. Stuur dit bericht 'Ik sta callmebot toe om mij berichten te sturen' (in het Engels) naar de nieuwe contactpersoon die is aangemaakt (uiteraard met behulp van Signal)",
+		"fr": "2. Envoyez ce message \"J'autorise callmebot à m'envoyer des messages\" (en anglais) au nouveau contact créé (en utilisant Signal bien sûr)",
+		"it": "2. Invia questo messaggio `Autorizzo callmebot a mandarmi messaggi` (in inglese) al nuovo contatto creato (usando ovviamente Signal)",
+		"es": "2. Envía este mensaje `Permito que callmebot me envíe mensajes` (en inglés) al nuevo contacto creado (usando Signal, por supuesto)",
+		"pl": "2. Wyślij tę wiadomość `Zezwalam callmebotowi na wysyłanie mi wiadomości` (w języku angielskim) do nowo utworzonego Kontaktu (oczywiście przy użyciu Signala)",
+		"zh-cn": "2. 将此消息“我允许 callmebot 向我发送消息”（英文）发送到创建的新联系人（当然使用 Signal）"
     },
     '3. Wait until you receive the message `API Activated for your phone number. Your APIKEY is 123123` from the bot. As this is still in beta testing, the activation can take up to 2 minutes.': {
-        'en': '3. Wait until you receive the message `API Activated for your phone number. Your APIKEY is 123123` from the bot. As this is still in beta testing, the activation can take up to 2 minutes.',
-        'de': '3. Warten Sie, bis Sie die Meldung `API Activated for your phone number. Your APIKEY is 123123` vom Bot. Da dies noch im Beta-Test ist, kann die Aktivierung bis zu 2 Minuten dauern.'
-    },
+		"en": "3. Wait until you receive the message `API Activated for your phone number. Your APIKEY is 123123` from the bot. As this is still in beta testing, the activation can take up to 2 minutes.",
+		"de": "3. Warten Sie, bis Sie die Meldung „API für Ihre Telefonnummer aktiviert“ erhalten. Ihr APIKEY ist 123123` vom Bot. Da sich dies noch im Beta-Test befindet, kann die Aktivierung bis zu 2 Minuten dauern.",
+		"ru": "3. Подождите, пока не появится сообщение «API активирован для вашего номера телефона». Ваш APIKEY 123123` от бота. Поскольку это все еще находится в стадии бета-тестирования, активация может занять до 2 минут.",
+		"pt": "3. Aguarde até receber a mensagem `API Ativada para seu número de telefone. Sua APIKEY é 123123` do bot. Como isso ainda está em teste beta, a ativação pode levar até 2 minutos.",
+		"nl": "3. Wacht tot u het bericht `API geactiveerd voor uw telefoonnummer ontvangt. Uw APIKEY is 123123` van de bot. Aangezien dit nog in bètatestfase is, kan de activering tot 2 minuten duren.",
+		"fr": "3. Attendez de recevoir le message `API activée pour votre numéro de téléphone. Votre APIKEY est à 123123` du bot. Comme il s'agit encore d'un test bêta, l'activation peut prendre jusqu'à 2 minutes.",
+		"it": "3. Attendi finché non ricevi il messaggio `API attivata per il tuo numero di telefono. La tua APIKEY è 123123` dal bot. Poiché è ancora in fase di beta test, l'attivazione può richiedere fino a 2 minuti.",
+		"es": "3. Espere hasta que reciba el mensaje `API activada para su número de teléfono. Su APIKEY es 123123` del bot. Como aún se encuentra en fase de prueba beta, la activación puede demorar hasta 2 minutos.",
+		"pl": "3. Poczekaj, aż pojawi się komunikat `API Aktywowano dla Twojego numeru telefonu. Twój klucz APIKEY to 123123` od bota. Ponieważ jest to jeszcze w fazie testów beta, aktywacja może potrwać do 2 minut.",
+		"zh-cn": "3. 等到您收到消息“API 已为您的电话号码激活”。您的 APIKEY 是来自机器人的 123123`。由于这仍处于 Beta 测试阶段，因此激活可能需要 2 分钟。"
+	},
     '4. The Signal message from the bot will contain the apikey needed to send messages using the API.': {
-        'en': '4. The Signal message from the bot will contain the apikey needed to send messages using the API.',
-        'de': '4. Die Signal-Nachricht vom Bot enthält die Apikey, die zum Senden von Nachrichten über die API erforderlich ist.'
-    },
+		"en": "4. The Signal message from the bot will contain the apikey needed to send messages using the API.",
+		"de": "4. Die Signalnachricht des Bots enthält den API-Schlüssel, der zum Senden von Nachrichten über die API erforderlich ist.",
+		"ru": "4. Сигнальное сообщение от бота будет содержать апиключ, необходимый для отправки сообщений через API.",
+		"pt": "4. A mensagem Signal do bot conterá o apikey necessário para enviar mensagens usando a API.",
+		"nl": "4. Het signaalbericht van de bot bevat de apikey die nodig is om berichten te verzenden met behulp van de API.",
+		"fr": "4. Le message Signal du bot contiendra la clé API nécessaire pour envoyer des messages à l'aide de l'API.",
+		"it": "4. Il messaggio Signal dal bot conterrà l'apikey necessaria per inviare messaggi utilizzando l'API.",
+		"es": "4. El mensaje Signal del bot contendrá la apikey necesaria para enviar mensajes usando la API.",
+		"pl": "4. Wiadomość Signal od bota będzie zawierać klucz apikey potrzebny do wysyłania wiadomości za pomocą API.",
+		"zh-cn": "4. 来自机器人的 Signal 消息将包含使用 API 发送消息所需的 apikey。"
+	},
     '5. You must enter the received key above.': {
-        'en': '5. You must enter the received key above.',
-        'de': '5. Sie müssen den empfangenen Schlüssel oben eingeben.'
+        "en": "5. You must enter the received key above.",
+		"de": "5. Sie müssen den erhaltenen Schlüssel oben eingeben.",
+		"ru": "5. Необходимо ввести полученный выше ключ.",
+		"pt": "5. Você deve inserir a chave recebida acima.",
+		"nl": "5. U moet de ontvangen sleutel hierboven invoeren.",
+		"fr": "5. Vous devez entrer la clé reçue ci-dessus.",
+		"it": "5. Devi inserire la chiave ricevuta sopra.",
+		"es": "5. Debe ingresar la clave recibida arriba.",
+		"pl": "5. Musisz wpisać otrzymany klucz powyżej.",
+		"zh-cn": "5. 您必须输入上面收到的密钥。"
     },
     'Detailed description how to get APIKEY could be found <a href=\'https://www.callmebot.com/blog/free-api-signal-send-messages/\' target=\'_blank\'>here</a>.': {
-        'en': 'Detailed description how to get APIKEY could be found <a href=\'https://www.callmebot.com/blog/free-api-signal-send-messages/\' target=\'_blank\'>here</a>.',
-        'de': 'Eine ausführliche Beschreibung, wie Sie APIKEY erhalten, finden Sie <a href=\'https://www.callmebot.com/blog/free-api-signal-send-messages/\' target=\'_blank\'>hier</a> .'
-    }
+		"en": "Detailed description how to get APIKEY could be found <a href=\\'https://www.callmebot.com/blog/free-api-signal-send-messages/\\' target=\\'_blank\\'>here</a>.",
+		"de": "Eine detaillierte Beschreibung, wie Sie APIKEY erhalten, finden Sie <a href=\\'https://www.callmebot.com/blog/free-api-signal-send-messages/\\' target=\\'_blank\\'>hier</a> .",
+		"ru": "Подробное описание получения APIKEY можно найти <a href=\\'https://www.callmebot.com/blog/free-api-signal-send-messages/\\' target=\\'_blank\\'>здесь</a> .",
+		"pt": "A descrição detalhada de como obter o APIKEY pode ser encontrada <a href=\\'https://www.callmebot.com/blog/free-api-signal-send-messages/\\' target=\\'_blank\\'>aqui</a> .",
+		"nl": "Gedetailleerde beschrijving hoe u APIKEY kunt krijgen, kunt u <a href=\\'https://www.callmebot.com/blog/free-api-signal-send-messages/\\' target=\\'_blank\\'>hier vinden</a> .",
+		"fr": "Une description détaillée de la façon d'obtenir APIKEY peut être trouvée <a href=\\'https://www.callmebot.com/blog/free-api-signal-send-messages/\\' target=\\'_blank\\'>ici</a> .",
+		"it": "Una descrizione dettagliata su come ottenere APIKEY può essere trovata <a href=\\'https://www.callmebot.com/blog/free-api-signal-send-messages/\\' target=\\'_blank\\'>qui</a> .",
+		"es": "Puede encontrar una descripción detallada de cómo obtener APIKEY <a href=\\'https://www.callmebot.com/blog/free-api-signal-send-messages/\\' target=\\'_blank\\'>aquí</a> .",
+		"pl": "Szczegółowy opis jak zdobyć APIKEY znajdziesz <a href=\\'https://www.callmebot.com/blog/free-api-signal-send-messages/\\' target=\\'_blank\\'>tutaj</a> .",
+		"zh-cn": "详细说明如何获取 APIKEY 可以在<a href=\\'https://www.callmebot.com/blog/free-api-signal-send-messages/\\' target=\\'_blank\\'>这里</a>找到。"
+	}
 };
